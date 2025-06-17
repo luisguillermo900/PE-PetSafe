1e0a7a37e631708d60801940082d63e5262ed701
luisguillermo900 <lchirinosco@unsa.edu.pe>
2025-06-17 03:04:40 -0500

    Standby historial

--- lib/views/historial/historial_view.dart
+++ lib/views/historial/historial_view.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../providers/providers.dart';
 
@@ -41,4 +41,4 @@ class HistorialView extends ConsumerWidget {
       },
     );
   }
-}
+}*/
